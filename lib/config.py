77eb5cd2cea582e9ca4f88754b2198b480a09a4e
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-01-21 14:20:14 +0700

    Update config.py

--- lib/config.py
+++ lib/config.py
@@ -1,1 +1,20 @@
+def kontol():
+	global toket
+	os.system('clear')
+	print ("Orang Yang Paling Ganteng😎 Adalah")
+	kontol()
+
+def kontol():
+	taik = raw_input('> ')
+	if taik =="":
+		print 'Slh kentod'
+
+ 	elif teak =="Yayan":
+		jalan("Lo Kontol tq ea")
+       
+	else:
+		print 'Slh kentod'
+		kontol()
+
+
 
